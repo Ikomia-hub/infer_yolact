446380cf44b2073bf964d5ca8826e69450005707
Ludovic Barusseau <ludovic.barusseau@ikomia.com>
2023-09-28 12:18:00 +0200

    Chore: remove description field + update git repos

--- infer_yolact_process.py
+++ infer_yolact_process.py
@@ -133,23 +133,6 @@ class InferYolactFactory(dataprocess.CTaskFactory):
         # Set process information as string here
         self.info.name = "infer_yolact"
         self.info.short_description = "A simple, fully convolutional model for real-time instance segmentation."
-        self.info.description = "We present a simple, fully-convolutional model for real-time (>30 fps) instance " \
-                                "segmentation that achieves competitive results on MS COCO evaluated on a single " \
-                                "Titan Xp, which is significantly faster than any previous state-of-the-art approach. " \
-                                "Moreover, we obtain this result after training on only one GPU. We accomplish this " \
-                                "by breaking instance segmentation into two parallel subtasks: (1) generating a set " \
-                                "of prototype masks and (2) predicting per-instance mask coefficients. Then we produce " \
-                                "instance masks by linearly combining the prototypes with the mask coefficients. " \
-                                "We find that because this process doesn't depend on repooling, this approach produces " \
-                                "very high-quality masks and exhibits temporal stability for free. Furthermore, we " \
-                                "analyze the emergent behavior of our prototypes and show they learn to localize " \
-                                "instances on their own in a translation variant manner, despite being fully-convolutional. " \
-                                "We also propose Fast NMS, a drop-in 12 ms faster replacement for standard NMS that " \
-                                "only has a marginal performance penalty. Finally, by incorporating deformable " \
-                                "convolutions into the backbone network, optimizing the prediction head with better " \
-                                "anchor scales and aspect ratios, and adding a novel fast mask re-scoring branch, " \
-                                "our YOLACT++ model can achieve 34.1 mAP on MS COCO at 33.5 fps, which is fairly " \
-                                "close to the state-of-the-art approaches while still running at real-time."
         # relative path -> as displayed in Ikomia application process tree
         self.info.path = "Plugins/Python/Segmentation"
         self.info.version = "1.3.0"
@@ -160,7 +143,8 @@ class InferYolactFactory(dataprocess.CTaskFactory):
         self.info.year = 2019
         self.info.license = "MIT License"
         self.info.documentation_link = "https://arxiv.org/abs/1912.06218"
-        self.info.repository = "https://github.com/dbolya/yolact"
+        self.info.repository = "https://github.com/Ikomia-hub/infer_yolact"
+        self.info.original_repository = "https://github.com/dbolya/yolact"
         self.info.keywords = "CNN,detection,instance,segmentation,semantic,resnet"
 
     def create(self, param=None):
